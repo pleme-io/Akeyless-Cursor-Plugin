488934a9073ff689252f9b158e5201197917a1e5
drzzln <drzzln@protonmail.com>
2026-03-15 20:52:30 -0400

    migrate to pleme-linker build via substrate mkTypescriptPackage
    Replaces devShell-only with working nix build. Uses pleme-linker for
    hermetic npm resolution (481 packages) and substrate's mkTypescriptPackage
    for the VS Code extension compilation.

--- flake.nix
+++ flake.nix
@@ -4,15 +4,34 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
+    substrate = {
+      url = "github:pleme-io/substrate";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pleme-linker = {
+      url = "github:pleme-io/pleme-linker";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, substrate, pleme-linker }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      substrateLib = substrate.libFor {
+        inherit pkgs system;
+      };
+
+      plemeLinkerPkg = pleme-linker.packages.${system}.default;
+
+      extension = substrateLib.mkTypescriptPackage {
+        name = "akeyless-cursor-plugin";
+        src = self;
+        plemeLinker = plemeLinkerPkg;
+      };
+
     in {
-      # NOTE: buildNpmPackage fails because keytar requires node-gyp with native
-      # platform libs (libsecret on Linux, Security.framework on macOS).
-      # Build with: npm install && npm run compile && vsce package
+      packages.default = extension;
 
       devShells.default = pkgs.mkShellNoCC {
         packages = with pkgs; [
@@ -20,6 +39,12 @@
           python3
           pkg-config
         ];
+        nativeBuildInputs = [ plemeLinkerPkg ];
+        shellHook = ''
+          echo "Akeyless Cursor Plugin development environment"
+          echo "  pleme-linker resolve --project . --include-dev  - Regenerate deps.nix"
+          echo "  nix build                                       - Build the extension"
+        '';
       };
     });
 }
