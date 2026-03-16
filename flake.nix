d0e4e233c8034184919d682e3229c9b379ef92a5
drzzln <drzzln@protonmail.com>
2026-03-15 21:19:54 -0400

    refactor: use substrate repo-flake.nix universal builder

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    flake-utils.url = "github:numtide/flake-utils";
     substrate = {
       url = "github:pleme-io/substrate";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,39 +11,18 @@
       url = "github:pleme-io/pleme-linker";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, substrate, pleme-linker }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-
-      substrateLib = substrate.libFor {
-        inherit pkgs system;
-      };
-
-      plemeLinkerPkg = pleme-linker.packages.${system}.default;
-
-      extension = substrateLib.mkTypescriptPackage {
-        name = "akeyless-cursor-plugin";
-        src = self;
-        plemeLinker = plemeLinkerPkg;
-      };
-
-    in {
-      packages.default = extension;
-
-      devShells.default = pkgs.mkShellNoCC {
-        packages = with pkgs; [
-          nodejs_22
-          python3
-          pkg-config
-        ];
-        nativeBuildInputs = [ plemeLinkerPkg ];
-        shellHook = ''
-          echo "Akeyless Cursor Plugin development environment"
-          echo "  pleme-linker resolve --project . --include-dev  - Regenerate deps.nix"
-          echo "  nix build                                       - Build the extension"
-        '';
-      };
-    });
+  outputs = inputs: (import "${inputs.substrate}/lib/repo-flake.nix" {
+    inherit (inputs) nixpkgs flake-utils;
+  }) {
+    self = inputs.self;
+    language = "typescript";
+    builder = "package";
+    pname = "akeyless-cursor-plugin";
+    plemeLinker = inputs.pleme-linker;
+    description = "VS Code / Cursor extension for managing Akeyless secrets and scanning for hardcoded secrets";
+    homepage = "https://github.com/pleme-io/Akeyless-Cursor-Plugin";
+  };
 }
